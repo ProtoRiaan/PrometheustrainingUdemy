c58bef718ba30d8f5167ad773a4d9e0db21b9f63
Riaan Schuld <rhschuld@gmail.com>
2023-04-30 19:11:51 +0000

    added uwsgi app to test with

--- httpserver_test.py
+++ httpserver_test.py
@@ -1,8 +1,11 @@
 import http.server
-from prometheus_client import start_http_server
+from prometheus_client import start_http_server, Counter
+
+REQUESTS = Counter('test_request_total', 'This is a counter test metric for Total Get requests')
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
+        REQUESTS.inc()
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b"test")
@@ -11,3 +14,5 @@ if __name__ == "__main__":
     start_http_server(5001)
     server =http.server.HTTPServer(('192.168.222.14', 5000), MyHandler)
     server.serve_forever()
+
+
